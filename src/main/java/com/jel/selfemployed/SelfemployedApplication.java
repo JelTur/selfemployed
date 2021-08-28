144e7fc433c0e7bab5ce93cc41fd8af685616d6f
JelTur <lenagalaxygold@gmail.com>
2021-08-28 15:30:07 +0300

    initial

---
+++ src/main/java/com/jel/selfemployed/SelfemployedApplication.java
@@ -0,0 +1,13 @@
+package com.jel.selfemployed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfemployedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfemployedApplication.class, args);
+	}
+
+}
